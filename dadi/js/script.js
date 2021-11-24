3562a91430c38fb33d9ccc3f4ea88563c7cc391f
Dandav2923 <daniele.emili23@gmail.com>
2021-11-24 14:21:26 +0100

    esercizio dadi

---
+++ dadi/js/script.js
@@ -0,0 +1,12 @@
+// alert ('tu madre');
+let userScore = Math.floor(Math.random() * 6);
+console.log(userScore);
+let pcScore = Math.floor(Math.random() * 6);
+console.log(pcScore);
+if (userScore > pcScore) {
+ console.log('Hai vinto barbone');
+}
+else if (userScore < pcScore ) {
+    console.log('Hai perso Stark');
+}else 
+console.log('Pari e patta');
