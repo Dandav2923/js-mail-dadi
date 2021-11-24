6998b78bf276f1fb4b487f32d844836af1dcfee9
Dandav2923 <daniele.emili23@gmail.com>
2021-11-24 15:48:12 +0100

    base degli esercizi

---
+++ mail/js/script.js
@@ -0,0 +1,15 @@
+const userMail = prompt('Give me your e-mail');
+const mailArray = ['luca@mario.it', 'luca@gmail.it', 'luca@yahoo.it', 'luca@tiscali.it', 'luca@live.it', 'luca@hotmail.it', 'luca@fulvio.it', 'luca@francesco.it',];
+let boolean = false;
+
+for (let index = 0; index < mailArray.length; index++) {
+   if (mailArray[index] == userMail) {
+    boolean = true;
+   }    
+};
+if (boolean == true) {
+    console.log(boolean);
+
+}
+else 
+console.log('non sei nella lista');
