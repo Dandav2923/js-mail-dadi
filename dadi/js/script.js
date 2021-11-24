839e6e951b19591c84766b8e3ac97272deefc55c
Dandav2923 <daniele.emili23@gmail.com>
2021-11-24 17:21:08 +0100

    commenti

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,21 +1,25 @@
 // alert ('tu madre');
+// dichiarazione variabili
 let userScore = Math.floor(Math.random() * 6) + 1;
 console.log(userScore);
 let pcScore = Math.floor(Math.random() * 6) +1 ;
 console.log(pcScore);
 const container = document.getElementById('container');
+const userScoreBox = document.querySelector('#container div');
+const pcScoreBox = document.querySelector('#container div:nth-child(2)');
+userScoreBox.innerHTML = userScore;
+pcScoreBox.innerHTML = pcScore;
+// /dichiarazione variabili
 
+// creazione box dove mettere gli elementi
 for (let index = 0; index < 3; index++) {
     const element = document.createElement('div');
     element.classList.add('box-dado');
     container.append(element);   
 }
+// /creazione box dove mettere gli elementi
 
-const userScoreBox = document.querySelector('#container div');
-const pcScoreBox = document.querySelector('#container div:nth-child(2)');
-userScoreBox.innerHTML = userScore;
-pcScoreBox.innerHTML = pcScore;
-
+// condizione per verificare il vincitore tra giocatore e pc 
 if (userScore > pcScore) {
     console.log('Hai vinto barbone'); 
     const result = document.querySelector('#container div:nth-child(3)');
@@ -33,3 +37,4 @@ else {
     const result = document.querySelector('#container div:nth-child(3)');
     result.innerHTML = 'Pareggio';
 }
+// /condizione per verificare il vincitore tra giocatore e pc
